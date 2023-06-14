58ac682f3dfcf87e3e55d922079572cb891ecff5
Lucas Gruner <lucas.gruner@gmail.com>
2023-06-14 17:19:08 +0200

    changed architecture to an app object with methods and properties

--- app.js
+++ app.js
@@ -1,36 +1,63 @@
-// Check if app.js is connected upon HTML load
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("app.js connected")
-});
+class App {
 
-// Open uploaded file
-const fileSelector = document.querySelector("#file-selector");
-fileSelector.addEventListener("change", (event) => {
-  console.log(event.target.files[0]);
-});
+  uploads = []
+  highlights = []
 
+  connect() {
+    // Check if app.js is connected upon HTML load
+    document.addEventListener("DOMContentLoaded", () => {
+      console.log("app.js connected")
+    });
 
-// Drag-and-drop area
-const dropArea = document.querySelector("#drop-area")
+    // Drag-and-drop area
+    const dropArea = document.querySelector("#drop-area")
+    dropArea.addEventListener("dragenter", (event) => {
+      dropArea.style.backgroundColor = "lightgreen";
+    });
 
-dropArea.addEventListener("dragenter", (event) => {
-  dropArea.style.backgroundColor = "green";
-});
+    dropArea.addEventListener("dragleave", (event) => {
+      dropArea.style.backgroundColor = "lightgrey";
+    });
 
-dropArea.addEventListener("dragleave", (event) => {
-  dropArea.style.backgroundColor = "lightgrey";
-});
+    dropArea.addEventListener('dragover', (event) => {
+      event.stopPropagation();
+      event.preventDefault();
+      event.dataTransfer.dropEffect = 'copy';
+    });
 
-dropArea.addEventListener('dragover', (event) => {
-  event.stopPropagation();
-  event.preventDefault();
-  event.dataTransfer.dropEffect = 'copy';
-});
+    dropArea.addEventListener("drop", (event) => {
+      event.stopPropagation();
+      event.preventDefault();
+      dropArea.style.backgroundColor = "lightgrey";
+      const files = event.dataTransfer.files;
+      for (let i = 0; i < files.length; i++) {
+        let file = files.item(i)
+        console.log(file);
+        console.log(file.name)
+        this.readFile(file);
+      }
+    });
+  }
 
-dropArea.addEventListener("drop", (event) => {
-  event.stopPropagation();
-  event.preventDefault();
-  dropArea.style.backgroundColor = "yellow";
-  const fileList = event.dataTransfer.files;
-  console.log(fileList[0]);
-});
+  readFile(file) {
+    const reader = new FileReader();
+    reader.addEventListener("load", (event) => {
+      const result = event.target.result;
+      const upload = {
+        name: file.name,
+        text: result
+      };
+      this.updateUploads(upload);
+    });
+    reader.readAsText(file);
+  }
+
+  updateUploads(upload) {
+    this.uploads.push(upload)
+    document.querySelector("#preview").innerText += upload.text;
+    console.log(`File '${upload.name}' imported. ${this.uploads.length} uploads in memory`);
+  }
+}
+
+const app = new App
+app.connect()
--- readme.md
+++ readme.md
@@ -2,4 +2,4 @@
 
 **WORK IN PROGRESS**
 
-A simple extractor for the highlights contained in the `my-clippings.txt` file of the Kindle. Developed for learning purposes.
+A simple extractor for the highlights contained in the `my-clippings.txt` file of the Kindle e-reader. Developed for learning purposes using vanilla HTML, CSS and JavaScript. Files are processed client-side (no upload to a server or storage).
